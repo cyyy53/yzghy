6d26ae937cabf4a281aa26045128461673a41b35
itcy <519618654@qq.com>
2023-08-28 10:03:15 +0800

    项目创建

---
+++ src/main/java/com/itcy/yzghy/yzghy_town/YzghyTownApplication.java
@@ -0,0 +1,13 @@
+package com.itcy.yzghy.yzghy_town;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YzghyTownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YzghyTownApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itcy/yzghy/yzghy_town/YzghyTownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcy.yzghy.yzghy_town;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YzghyTownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
